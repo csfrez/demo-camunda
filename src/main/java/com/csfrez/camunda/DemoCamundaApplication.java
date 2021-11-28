763ae328da6c33b9c454db5568be2079f9cef82c
Frez <csfrez@163.com>
2021-11-28 20:54:52 +0800

    初始化

---
+++ src/main/java/com/csfrez/camunda/DemoCamundaApplication.java
@@ -0,0 +1,13 @@
+package com.csfrez.camunda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCamundaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCamundaApplication.class, args);
+	}
+
+}
